173d1c55751e9ffe15c30f829967e2a89af941a3
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2021-11-04 11:49:41 -0400

    Add files via upload

--- lipstick_page.js
+++ lipstick_page.js
@@ -1,3 +1,7 @@
+noseX=0;
+noseY=0;
+
+
 function tohome(){
     window.location="main_page.html"
 }
@@ -9,10 +13,19 @@ function setup(){
     video= createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
 }
 
 
-function take_snapshot(){
+
+function modelLoaded(){
+    console.log("PoseNet Initialized");
+
+}
+
+
+function take_snapshot_two(){
     save("lipstickfilter.png");
 }
 
@@ -20,3 +33,17 @@ function take_snapshot(){
 function draw(){
     image(video,0 ,0, 300, 300);    
 }
+
+
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-30;
+        noseY=results[0].pose.nose.y-25;
+        console.log("Lipstick X -- "+noseX);
+        console.log("Lipstick Y -- "+noseY);
+   
+    }
+   }
+
+
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+
+
+
 function gohome(){
     window.location="main_page.html"
 }
@@ -8,6 +13,8 @@ function setup(){
     video= createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
 }
 
 
@@ -16,7 +23,26 @@ function take_snapshot(){
 }
 
 
+function modelLoaded(){
+    console.log("PoseNet Initialized");
+
+}
+
+
+
 function draw(){
     image(video,0 ,0, 300, 300);
     
 }
+
+
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-30;
+        noseY=results[0].pose.nose.y-25;
+        console.log("Mustache X -- "+noseX);
+        console.log("Mustache Y -- "+noseY);
+   
+    }
+   }
